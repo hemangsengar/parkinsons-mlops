63872652593414cf5bafd4946607825a7cb1a6ef
Hemang Singh Sengar <121383204+hemangsengar@users.noreply.github.com>
2025-12-03 09:36:02 +0530

    Update inference.py

--- src/inference.py
+++ src/inference.py
@@ -1,10 +1,15 @@
-
 from src.model import ParkinsonModel
 from src.preprocess import preprocess_input
+import yaml
 
-model = ParkinsonModel()
+def run_inference(input_data: dict):
+    with open("config/config.yaml", "r") as f:
+        config = yaml.safe_load(f)
 
-def predict_parkinson(input_dict):
-    features = preprocess_input(input_dict)
-    pred = model.predict(features)
-    return "Parkinson's" if pred == 1 else "Healthy"
+    features = config["features"]
+
+    df = preprocess_input(input_data, features)
+    model = ParkinsonModel()
+    prediction = model.predict(df)
+
+    return prediction
