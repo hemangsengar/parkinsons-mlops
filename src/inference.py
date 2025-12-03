bff8c9f71728ac2fae06dd18bfeea907c22b46ef
Hemang Singh Sengar <121383204+hemangsengar@users.noreply.github.com>
2025-12-03 07:16:52 +0530

    Update inference.py

--- src/inference.py
+++ src/inference.py
@@ -1,1 +1,10 @@
 
+from src.model import ParkinsonModel
+from src.preprocess import preprocess_input
+
+model = ParkinsonModel()
+
+def predict_parkinson(input_dict):
+    features = preprocess_input(input_dict)
+    pred = model.predict(features)
+    return "Parkinson's" if pred == 1 else "Healthy"
