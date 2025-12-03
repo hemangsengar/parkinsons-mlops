6ba775a56e251fc5960ca579f6b930b4bb6039ac
Hemang Singh Sengar <121383204+hemangsengar@users.noreply.github.com>
2025-12-03 07:16:21 +0530

    Update model.py

--- src/model.py
+++ src/model.py
@@ -1,1 +1,9 @@
+import joblib
+
+class ParkinsonModel:
+    def __init__(self, model_path="models/model.pkl"):
+        self.model = joblib.load(model_path)
+
+    def predict(self, features):
+        return self.model.predict([features])[0]
 
