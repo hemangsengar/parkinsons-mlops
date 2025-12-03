397b1095654147dbe3431f608d0a98b19725f1da
Hemang Singh Sengar <121383204+hemangsengar@users.noreply.github.com>
2025-12-03 20:09:26 +0530

    Update train_colab.py

--- notebooks/train_colab.py
+++ notebooks/train_colab.py
@@ -1,1 +1,25 @@
-# training code here
+!pip install pandas numpy scikit-learn joblib
+from google.colab import files
+uploaded = files.upload()
+os.rename("parkinsons (1).data", "parkinsons.csv")
+import pandas as pd
+df = pd.read_csv("parkinsons.csv")
+df.head()
+df = df.drop("name", axis=1)
+X = df.drop("status", axis=1)
+y = df["status"]
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
+from sklearn.ensemble import RandomForestClassifier
+model = RandomForestClassifier()
+model.fit(X_train, y_train)
+from sklearn.metrics import accuracy_score
+pred = model.predict(X_test)
+accuracy_score(y_test, pred)
+import joblib
+joblib.dump(model, "model.pkl")
+
+
+
